b2f4c79bd04ef7340f0d1517e53af4f439ace49c
Destruidoss <natal_sasuke@outlook.com>
2025-10-08 17:15:57 +0100

    commit testing

--- fundamentals/string_methods.rb
+++ fundamentals/string_methods.rb
@@ -1,5 +1,5 @@
 name = "alo".reverse.upcase
-#e lido sempre de cima para baixo e a execucao e feita por linhas
+#e lido sempre de cima para baixo e a execucao e feita por linha
 puts name.reverse
 
 name = "joao".reverse.downcase
