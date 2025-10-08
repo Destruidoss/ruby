a42d583b5669f38df09ae542691dafae70939ab6
Destruidoss <natal_sasuke@outlook.com>
2025-10-08 16:56:56 +0100

    Commit initial exercicies

---
+++ fundamentals/comments.rb
@@ -0,0 +1,7 @@
+#this comment is only one line long!
+
+=begin
+this comment
+takes several
+lines!
+=end
---
+++ fundamentals/string_methods.rb
@@ -0,0 +1,7 @@
+name = "alo".reverse.upcase
+#e lido sempre de cima para baixo e a execucao e feita por linhas
+puts name.reverse
+
+name = "joao".reverse.downcase
+
+puts name.reverse
