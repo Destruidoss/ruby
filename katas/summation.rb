e6ff146e5257d7db07ade55e96004a3346e68278
Destruidoss <natal_sasuke@outlook.com>
2025-10-12 18:32:56 +0000

    commit for addkatas

--- katas/summation.rb
+++ katas/summation.rb
@@ -8,3 +8,24 @@ end
 
 puts summation(2)
 
+=begin
+outra formas de resolver
+
+def summation(num)
+  i = 1
+  sum = 0
+  while i <= num do
+    sum += i
+    i += 1
+  end
+  sum  
+end
+
+def summation(num)
+  (1..num).sum
+end
+
+
+enunciado
+Escreva um programa que encontre a soma de todos os números de 1 a num (ambos inclusivos). O número será sempre um inteiro positivo maior que 0. Sua função precisa retornar apenas o resultado.
+=end
