abb71936ff656eeea580aef5ab1dc628f3d57a11
Destruidoss <natal_sasuke@outlook.com>
2025-10-21 15:56:05 +0100

    commit finished codeacademy

---
+++ rails/rubyonrails.rb
@@ -0,0 +1,29 @@
+############criando pagina######################
+=begin 
+#########111111########
+Vamos começar criando um aplicativo Rails para um site pessoal. Explicaremos cada etapa no próximo exercício.
+
+No terminal, digite:
+
+$ rails new MySite 
+
+#########22222222########2
+Em seguida, navegue até o diretório MySite usando:
+
+$ cd MySite
+
+#########3333333########2
+Agora, para instalar todas as dependências, use:
+
+$ bundle install
+
+#########444444########2
+Por fim, para inicializar o servidor, digite:
+
+$ rails server
+
+#########555555555########2
+Em seguida, visualize o aplicativo Rails visitando ou atualizando http://localhost:4001no navegador fornecido.
+
+Você deveria ver o logotipo do Ruby on Rails!
+=end
