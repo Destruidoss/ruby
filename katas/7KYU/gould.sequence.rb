5088878c3fa0153b4fcdb546d80fe13fbf3a12be
Destruidoss <natal_sasuke@outlook.com>
2025-10-25 11:25:05 +0000

    kata 7kyu gould sequence

--- katas/7KYU/gould.sequence.rb
+++ katas/7KYU/gould.sequence.rb
@@ -2,12 +2,14 @@ def gould
   yield "yield something"
 end
 
-def gould
-  Enumerator.new do |elder| 
+def gould2
+  Enumerator.new do |el| 
     n = 0
     loop do
-      elder.yield n.to_s(2).count('1')
+      el.yield n.to_s(2).count('1')
       n += 1
     end
   end
 end
+
+
